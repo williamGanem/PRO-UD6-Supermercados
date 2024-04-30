f72edb5359ee9c93cba1c2c84a2b532451af66df
williamGanem <usr3852@salesianos-lacuesta.net>
2024-04-30 13:31:14 +0100

    feat Creacion de la clase Cesta

---
+++ src/com/supermercado/cesta/Cesta.java
@@ -0,0 +1,13 @@
+package com.supermercado.cesta;
+
+import java.util.Stack;
+
+public class Cesta {
+    private Stack<String> pilaProductos;
+
+    public Cesta() {
+        this.pilaProductos = new Stack<>();
+        int totalProductos = 4;
+    }
+
+}
